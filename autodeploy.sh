a78bbb369f3db747dd81e834f23e253d48de1c9e
root <root@WFGN001.cs1local>
2017-01-15 20:07:49 +0000

    Auto deploy script added.

---
+++ autodeploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd <path>
+git pull
+
+cd configurator-ui
+pub get
+pub build
+docker stop pcbuilder-frontend
+docker build -t pcbuilder/frontend .
+docker run --name "pcbuilder-frontend" -d -e "BACKEND_ADDRESS=<>" -p 80:80 -t pcbuilder/frontend
