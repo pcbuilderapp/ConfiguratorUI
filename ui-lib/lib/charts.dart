4bc331c4da98c8f0a9e79f1c09452e1e286e89d7
Stefan Ladage <sladage@gmail.com>
2017-01-25 22:57:47 +0100

    Charts with language setting.

--- ui-lib/lib/charts.dart
+++ ui-lib/lib/charts.dart
@@ -1,17 +1,17 @@
 import 'dart:html';
+import 'dart:async';
 import 'package:google_charts/google_charts.dart';
 import 'package:pcbuilder.api/domain/pricepoint.dart';
 
-drawLineChart(List<PricePoint> pricePoints, Element lineChartElement) {
+drawLineChart(List<PricePoint> pricePoints, Element lineChartElement) async {
+  await Chart.load(packages:['corechart'],language:"nl");
+  var rawData = [["Date","Price"]];
+  for (PricePoint pricePoint in pricePoints) {
+    rawData.add([new DateTime.fromMillisecondsSinceEpoch(pricePoint.pricingDate), pricePoint.price]);
+  }
+  var data = arrayToDataTable(rawData, false);
+  var chart = new LineChart(lineChartElement);
+  var options = {'title': 'Price History','backgroundColor': '#f1f3f4','vAxis':{'format':'currency'}};
+  chart.draw(data, options);
 
-  LineChart.load().then((_) {
-    var rawData = [["Date","Price"]];
-    for (PricePoint pricePoint in pricePoints) {
-      rawData.add([new DateTime.fromMillisecondsSinceEpoch(pricePoint.pricingDate), pricePoint.price]);
-    }
-    var data = arrayToDataTable(rawData, false);
-    var chart = new LineChart(lineChartElement);
-    var options = {'title': 'Price History','backgroundColor': '#f1f3f4'};
-    chart.draw(data, options);
-  });
 }
